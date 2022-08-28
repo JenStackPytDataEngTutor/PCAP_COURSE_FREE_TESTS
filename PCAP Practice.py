02b7adfc66f3a61bd6b94c55d2cd162c58d89cff
Jennifer Stack Lead Tutor <jstack@jenniferstackgis.onmicrosoft.com>
2022-08-28 20:31:19 +0100

    Delete PCAP Practice.py

--- PCAP Practice.py
+++
@@ -1,6 +0,0 @@
-phrases = ['when in rome', 
-    'what goes around comes around', 
-    'all is fair in love and war'
-    ]
-phrases.sort(key=lambda x: x.split()[0][1], reverse=True)
-print(phrases)
